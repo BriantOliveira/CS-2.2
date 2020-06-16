618f032f2cb9b2410551a32c06e8029bec43e93f
Elliot Briant <briant.deoliveira@students.dominican.edu>
2020-06-15 19:37:56 -0700

    fixing a few bugs

--- util/file_reader.py
+++ util/file_reader.py
@@ -19,17 +19,17 @@ def read_graph_from_file(filename):
 
     # TODO: Use the first line (G or D) to determine whether graph is directed
     # and create a graph object
-    graph_directed = True if f[0] == "D" else False
-    graph_obj = Graph(graph_directed=graph_directed)
+    is_directed = True if f[0] == "D" else False
+    graph_obj = Graph(is_directed=is_directed)
 
     # TODO: Use the second line to add the vertices to the graph
     graph_vertices = f[1].split(',')
-    for vertice in vertices:
+    for vertice in graph_vertices:
         graph_obj.add_vertex(vertice)
 
     # TODO: Use the 3rd+ line to add the edges to the graph
 
-    add_edges = f[2:]
+    edges = f[2:]
     for edge in edges:
         edge1, edge2 = edge.string(')(').split(',')
         graph_obj.add_edge(edge1, edge2)
@@ -38,7 +38,6 @@ def read_graph_from_file(filename):
 
 
 if __name__ == '__main__':
-
     graph = read_graph_from_file('test.txt')
 
     print(graph)
